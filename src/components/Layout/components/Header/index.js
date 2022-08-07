66a69fccac1ff6acaf7f5d06c708eea0ba8faec6
phminh3033 <phminh3033@gmail.com>
2022-08-07 16:04:38 +0700

    build Layout base

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,5 +1,14 @@
+import classNames from 'classnames/bind'; //Allows to write class names with '-' => Ex: post-item
+import styles from './Header.module.scss';
+
+const cx = classNames.bind(styles);
+
 function Header() {
-    return <h1>Header</h1>;
+    return (
+        <header className={cx('wrapper')}>
+            <div className={cx('inner')}></div>
+        </header>
+    );
 }
 
 export default Header;
