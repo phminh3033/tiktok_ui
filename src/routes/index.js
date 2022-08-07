e74ede26fec564cf88550622c993af39d092ef0e
phminh3033 <phminh3033@gmail.com>
2022-08-07 15:07:32 +0700

    config Router & Layout

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,18 @@
+//Layouts
+import { HeaderOnly } from '~/components/Layout';
+
+//Pages
 import Home from '~/pages/Home';
 import Following from '~/pages/Following';
+import Profile from '~/pages/Profile';
+import Upload from '~/pages/Upload';
 
 //Can see until don't login
 const publicRoutes = [
     { path: '/', component: Home },
     { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
 ];
 
 //Must be login
